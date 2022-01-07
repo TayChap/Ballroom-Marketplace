331437052e20cea27510508960f346fe386026dc
Taylor Chapman <tchapman@headcheckhealth.com>
2022-01-07 15:43:50 -0800

    added template files

---
+++ BallroomBuySell/Models/SaleItem/SaleItemCellStructure.swift
@@ -0,0 +1,16 @@
+//
+//  SaleItemCellStructure.swift
+//  BallroomBuySell
+//
+//  Created by Taylor Chapman on 2022-01-07.
+//
+
+struct SaleItemCellStructure {
+    var type: SaleItemCellType
+    var dataType: String
+    var serverKey: String
+    var title: String
+    var placeholder: String
+    var values: [[ Any ]]
+    var required: Bool
+}
---
+++ BallroomBuySell/Models/SaleItem/SaleItemTemplate.swift
@@ -0,0 +1,12 @@
+//
+//  SaleItemTemplate.swift
+//  BallroomBuySell
+//
+//  Created by Taylor Chapman on 2022-01-07.
+//
+
+struct SaleItemTemplate {
+    var name: String
+    var previewCellStructure: [SaleItemPreviewCellStructure]
+    var screenStructure: [SaleItemCellStructure]
+}
