bff1c01fe7101353f4f6837943cc8fbb2026abcf
Taylor Chapman <tchapman@headcheckhealth.com>
2022-03-30 10:15:37 -0700

    permissions localization

--- BallroomBuySell/Managers/PermissionsManager.swift
+++ BallroomBuySell/Managers/PermissionsManager.swift
@@ -10,16 +10,31 @@ import Photos
 import ContactsUI
 
 struct PermissionManager {
+    enum PermissionsType {
+        case camera, photos
+        
+        var permissionsRequestString: String {
+            String(format: LocalizedString.string("permissions.request"), LocalizedString.string(self.appStringKey))
+        }
+        
+        private var appStringKey: String {
+                switch self {
+                case .camera: return "apple.camera.app"
+                case .photos: return "apple.photos.app"
+                }
+        }
+    }
+    
     static func checkCameraPermissions(owner: UIViewController?, _ completion: @escaping () -> Void) {
         switch AVCaptureDevice.authorizationStatus(for: .video) {
         case .authorized:
             completion()
         case .denied, .restricted:
-            PermissionManager.showAlert(owner, withMessage: "permissions_camera")
+            PermissionManager.showAlert(owner, withMessage: PermissionsType.camera.permissionsRequestString)
         default:
             AVCaptureDevice.requestAccess(for: .video) { (hasAccess) in
                 if !hasAccess {
-                    PermissionManager.showAlert(owner, withMessage: "permissions_camera")
+                    PermissionManager.showAlert(owner, withMessage: PermissionsType.camera.permissionsRequestString)
                 } else {
                     completion()
                 }
@@ -33,14 +48,14 @@ struct PermissionManager {
         case .authorized:
             completion()
         case .denied, .restricted:
-            PermissionManager.showAlert(owner, withMessage: "permissions_gallery")
+            PermissionManager.showAlert(owner, withMessage: PermissionsType.photos.permissionsRequestString)
         default:
             PHPhotoLibrary.requestAuthorization { (status) in
                 switch status {
                 case .authorized:
                     completion()
                 default:
-                    PermissionManager.showAlert(owner, withMessage: "permissions_gallery")
+                    PermissionManager.showAlert(owner, withMessage: PermissionsType.photos.permissionsRequestString)
                 }
             }
         }
@@ -48,9 +63,8 @@ struct PermissionManager {
     
     // MARK: - Private Helpers
     private static func showAlert(_ vc: UIViewController?, withMessage message: String) {
-        let cancel = UIAlertAction(title: "generic.cancel", style: .default, handler: nil)
-
-        let setting = UIAlertAction(title: "navigation.item.settings.title", style: .default) { (action) in
+        let cancel = UIAlertAction(title: LocalizedString.string("generic.cancel"), style: .default, handler: nil)
+        let setting = UIAlertAction(title: LocalizedString.string("apple.settings.app"), style: .default) { (action) in
             if let url = URL(string: UIApplication.openSettingsURLString) {
                 UIApplication.shared.open(url, options: [.universalLinksOnly : false], completionHandler: nil)
             }
