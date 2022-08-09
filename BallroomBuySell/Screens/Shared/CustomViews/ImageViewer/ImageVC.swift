d4ba26c50892fc569e68a37fb1bb84dad68be051
Taylor Chapman <tchapman@headcheckhealth.com>
2022-08-09 11:18:15 -0700

    page view solid

--- BallroomBuySell/Screens/Shared/CustomViews/ImageViewer/ImageVC.swift
+++ BallroomBuySell/Screens/Shared/CustomViews/ImageViewer/ImageVC.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ImageVC: UIViewController, UIScrollViewDelegate {
-    @IBOutlet weak var scrollView: UIScrollView!
+class ImageVC: UIViewController/*, UIScrollViewDelegate*/ {
+//    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
     
     private var image: UIImage!
     
@@ -29,36 +29,36 @@ class ImageVC: UIViewController, UIScrollViewDelegate {
         imageView.image = image
     }
     
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        updateMinZoomScaleForSize(view.bounds.size)
-    }
-    
-    // MARK: - UIScrollView Delegate
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        imageView
-    }
-
-    func updateMinZoomScaleForSize(_ size: CGSize) {
-        let widthScale = size.width / imageView.bounds.width
-        let heightScale = size.height / imageView.bounds.height
-        let minScale = min(widthScale, heightScale)
-
-        scrollView.minimumZoomScale = minScale
-        scrollView.zoomScale = minScale
-    }
-
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        let viewSize = view.bounds.size
-
-        let yOffset = max(0, (viewSize.height - imageView.frame.height) / 2)
-        imageViewTopConstraint.constant = yOffset
-        imageViewBottomConstraint.constant = yOffset
-
-        let xOffset = max(0, (viewSize.width - imageView.frame.width) / 2)
-        imageViewLeadingConstraint.constant = xOffset
-        imageViewTrailingConstraint.constant = xOffset
-
-        view.layoutIfNeeded()
-    }
+//    override func viewWillLayoutSubviews() {
+//        super.viewWillLayoutSubviews()
+//        updateMinZoomScaleForSize(view.bounds.size)
+//    }
+//
+//    // MARK: - UIScrollView Delegate
+//    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+//        imageView
+//    }
+//
+//    func updateMinZoomScaleForSize(_ size: CGSize) {
+//        let widthScale = size.width / imageView.bounds.width
+//        let heightScale = size.height / imageView.bounds.height
+//        let minScale = min(widthScale, heightScale)
+//
+//        scrollView.minimumZoomScale = minScale
+//        scrollView.zoomScale = minScale
+//    }
+//
+//    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+//        let viewSize = view.bounds.size
+//
+//        let yOffset = max(0, (viewSize.height - imageView.frame.height) / 2)
+//        imageViewTopConstraint.constant = yOffset
+//        imageViewBottomConstraint.constant = yOffset
+//
+//        let xOffset = max(0, (viewSize.width - imageView.frame.width) / 2)
+//        imageViewLeadingConstraint.constant = xOffset
+//        imageViewTrailingConstraint.constant = xOffset
+//
+//        view.layoutIfNeeded()
+//    }
 }
